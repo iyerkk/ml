329a79f65f97bd61bd9f8bec2603632fd408dd63
iyerkk <karthikkannan123@gmail.com>
2019-08-03 16:15:12 -0700

    fix download historical prices

--- MachineLearningStocks/readfile.py
+++ MachineLearningStocks/readfile.py
@@ -2,10 +2,10 @@ import csv
 csvfile = open('SNP500.csv', 'rt')
 csvReader = csv.reader(csvfile, delimiter=",")
 csvreader = csv.reader(csvfile)
-d = dict()
+#d = dict()
 l =  list()
 for row in csvReader:
-	d[row[1]] = row[0]
-	l.append((row[0], row[1]))
-print(d)
+	#d[row[1]] = row[0]
+	l.append((row[2]))
+#print(d)
 print(l)
